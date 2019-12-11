9f18b94292cb3bdcbcac96b271fc6a2a794bb22b
Carolina <carolina.jimenez.g@gmail.com>
2019-12-11 16:09:02 -0300

    Creates About component

--- web/src/App.js
+++ web/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Header from './components/Header/Header';
+import About from './components/About/About';
 
 const navElems = [
   {
@@ -31,6 +32,7 @@ function App() {
     <div className="App">
       <Navbar navElems={navElems} />
       <Header navElems={navElems} />
+      <About />
     </div>
   );
 }
---
+++ web/src/components/About/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './About.css';
+
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="About">
+      </div>
+    );
+  }
+}
+export default About;
