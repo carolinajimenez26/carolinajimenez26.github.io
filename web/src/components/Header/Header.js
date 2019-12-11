f1a9d6073b09cc0a9dc4699b1b2b876549c08912
Carolina <carolina.jimenez.g@gmail.com>
2019-12-11 15:19:45 -0300

    changes color in Header and NavHeader and aligns text in the center

--- web/src/components/Header/Header.js
+++ web/src/components/Header/Header.js
@@ -6,7 +6,15 @@ class Header extends React.Component {
   render() {
     return (
       <div className="Header">
-        <HeaderNav />
+        <div className="row">
+          <HeaderNav />
+        </div>
+        <div className="row d-flex justify-content-center" id="text-elem">
+          <h1 className="text-white">Hello, this is Carolina</h1>
+          <span className="text-white">
+            ---- I'm a Software Developer ----
+          </span>
+        </div>
       </div>
     );
   }
