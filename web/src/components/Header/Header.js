5c703fcad4602486cea9d64390d153c586006447
Carolina <carolina.jimenez.g@gmail.com>
2019-12-11 16:04:53 -0300

    Adds social links

--- web/src/components/Header/Header.js
+++ web/src/components/Header/Header.js
@@ -5,10 +5,40 @@ import HeaderNav from '../HeaderNav/HeaderNav'
 class Header extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      socialElems: []
+    };
+  }
+
+  componentDidMount() {
+    let socialElems = [
+      {
+        "name": "twitter",
+        "className": "fa fa-twitter",
+        "href": "https://twitter.com/carojimenez26_"
+      },
+      {
+        "name": "medium",
+        "className": "fa fa-medium",
+        "href": "https://medium.com/@carolinajimenez26"
+      },
+    ];
+    this.setState({ socialElems });
   }
 
   render() {
+    let socialItems = [];
+
+    for (const [index, value] of this.state.socialElems.entries()) {
+      socialItems.push(
+        <li key={index}>
+          <a className="text-white" href={value.href} target="_blank">
+            <i className={value.className}></i>
+          </a>
+        </li>
+      );
+    }
+
     return (
       <div className="Header">
         <div className="row">
@@ -19,6 +49,11 @@ class Header extends React.Component {
           <span className="text-white">
             ---- I'm a Software Developer ----
           </span>
+        </div>
+        <div className="d-none d-lg-block" id="social-links">
+          <ul>
+            {socialItems}
+          </ul>
         </div>
       </div>
     );
