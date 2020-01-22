2d379f0622f265ea5d1305e7233d6209d462b16f
Carolina <carolina.jimenez.g@gmail.com>
2020-01-22 15:40:32 -0300

    Updates

---
+++ precache-manifest.7091d06c575fa2007f72a40e423890c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1797190752b10d110d124052c68591d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9293aa3f82ee8373bfcf",
+    "url": "/static/css/main.5aae0c0c.chunk.css"
+  },
+  {
+    "revision": "63d5bd66296844088d49",
+    "url": "/static/js/2.f61c10bd.chunk.js"
+  },
+  {
+    "revision": "bf91befff6f03511a9fca9de4a4d613f",
+    "url": "/static/js/2.f61c10bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9293aa3f82ee8373bfcf",
+    "url": "/static/js/main.7af810e3.chunk.js"
+  },
+  {
+    "revision": "9d892c65bfdc7fb83e1a",
+    "url": "/static/js/runtime-main.d1d5d272.js"
+  }
+]);
